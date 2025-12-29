c0c969cd38f6d21c56ecb796ca76bb28b69ef27d
Klaye May <makuntimacaleb@gmail.com>
2025-12-29 18:04:37 +0100

    V1.3 merde

--- merde.py
+++ merde.py
@@ -1,16 +1,16 @@
 class mer:
-    def ___init__(self, mon="lui", nom2="po"):
+    def __init__(self, mon="lui", nom2="po"):
         self.mon = mon
         self.nom2 = nom2
     def getnom(self):
         return self.mon
-    """def setnom(self,nom):
-        self.mon = nom"""
+    def setnom(self,nom):
+        self.mon = nom
 
 lui = "pop"
 lo = "va chier"
-monp = mer(lui)
+monp = mer(lui, lo)
 
 print(monp.getnom(), monp.nom2)
-"""monp.setnom("connard")"""
+monp.setnom("connard")
 print(monp.getnom())
